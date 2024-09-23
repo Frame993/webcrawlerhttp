72f3282c90165267bfb7db64ad43c5e52fe5f7bf
Yordani <yalvarezb93@gmail.com>
2024-09-23 20:20:12 +0300

    add crawl single page with error handling

--- crawl.js
+++ crawl.js
@@ -1,26 +1,50 @@
 const { JSDOM } = require("jsdom");
 
+async function crawlPage(currentURL) {
+  console.log(`actively crawling ${currentURL}`);
+  try {
+    const resp = await fetch(currentURL);
+    if (resp.status > 399) {
+      console.log(
+        `error in fetch with status code: ${resp.status}, on page ${currentURL}`
+      );
+      return;
+    } 
+   
+    const contentType = resp.headers.get("content-type");
+    if (!contentType.includes("text/html")) {
+      console.log(
+        `non html response, content type: ${contentType}, on page ${currentURL}`
+      );
+      return;
+    }
+    console.log(await resp.text());
+  } catch (err) {
+    console.log(`error with ${currentURL}: ${err.message}`);
+  }
+}
+
 function getURlsFromHTML(htmlBody, baseURL) {
   const urls = [];
   const dom = new JSDOM(htmlBody);
   const linkElements = dom.window.document.querySelectorAll("a");
   for (const linkElement of linkElements) {
     if (linkElement.href.slice(0, 1) === "/") {
-        // relative
-        try {
-            const urlObj = new URL(`${baseURL}${linkElement.href}`);
-            urls.push(urlObj.href);
-        } catch (err) {
-            console.log(`Error with relative url ${err.message}`);
-        }
+      // relative
+      try {
+        const urlObj = new URL(`${baseURL}${linkElement.href}`);
+        urls.push(urlObj.href);
+      } catch (err) {
+        console.log(`Error with relative url ${err.message}`);
+      }
     } else {
-        // absolute
-        try {
-          const urlObj = new URL(linkElement.href);
-          urls.push(urlObj.href);
-        } catch (err) {
-          console.log(`Error with absolute url ${err.message}`);
-        }
+      // absolute
+      try {
+        const urlObj = new URL(linkElement.href);
+        urls.push(urlObj.href);
+      } catch (err) {
+        console.log(`Error with absolute url ${err.message}`);
+      }
     }
   }
   return urls;
@@ -38,4 +62,5 @@ function normalizeURL(urlString) {
 module.exports = {
   normalizeURL,
   getURlsFromHTML,
+  crawlPage,
 };
