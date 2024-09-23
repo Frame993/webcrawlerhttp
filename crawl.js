12505819e58d23735990076dbf915c39c5071cf5
Yordani <yalvarezb93@gmail.com>
2024-09-23 21:04:04 +0300

    add crawl page recursive

--- crawl.js
+++ crawl.js
@@ -1,27 +1,51 @@
 const { JSDOM } = require("jsdom");
 
-async function crawlPage(currentURL) {
+async function crawlPage(baseURL, currentURL, pages) {
+  
+  const baseURLObj = new URL(baseURL);
+  const currentURLObj = new URL(currentURL);
+  if (baseURLObj.hostname !== currentURLObj.hostname) {
+    return pages;
+  }
+  
+  const normalizedCurrentURL = normalizeURL(currentURL);
+  if (pages[normalizedCurrentURL] > 0) {
+    pages[normalizedCurrentURL]++
+    return pages;
+  }
+  
+  pages[normalizedCurrentURL] = 1
   console.log(`actively crawling ${currentURL}`);
+  
+
   try {
     const resp = await fetch(currentURL);
     if (resp.status > 399) {
       console.log(
         `error in fetch with status code: ${resp.status}, on page ${currentURL}`
       );
-      return;
-    } 
-   
+      return pages;
+    }
+
     const contentType = resp.headers.get("content-type");
     if (!contentType.includes("text/html")) {
       console.log(
         `non html response, content type: ${contentType}, on page ${currentURL}`
       );
-      return;
+      return pages;
     }
-    console.log(await resp.text());
+
+    const htmlBody = await resp.text()
+    const nextURLs = getURlsFromHTML(htmlBody, baseURL)
+
+    for (const nextURL of nextURLs){
+      pages = await crawlPage(baseURL, nextURL, pages)
+    }
+
   } catch (err) {
     console.log(`error with ${currentURL}: ${err.message}`);
   }
+  return pages 
 }
 
 function getURlsFromHTML(htmlBody, baseURL) {
@@ -51,7 +75,7 @@ function getURlsFromHTML(htmlBody, baseURL) {
 }
 
 function normalizeURL(urlString) {
-  const urlObj = new URL(urlString); // new URL already checks case sensitive
+  const urlObj = new URL(urlString); // new URL method already checks case sensitive
   const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
   if (hostPath.length > 0 && hostPath.slice(-1) === "/") {
     return hostPath.slice(0, -1);
